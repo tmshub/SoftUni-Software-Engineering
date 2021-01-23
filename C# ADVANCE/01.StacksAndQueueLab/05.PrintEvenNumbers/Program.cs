b8936d9ed198e191a5dc4b6f8d251d617fc15d78
tmshub <tmshub@abv.bg>
2021-01-23 19:21:48 +0200

    resolved problem

---
+++ C# ADVANCE/01.StacksAndQueueLab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queueOfDigits = new Queue<int>(input.Length);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 == 0)
+                {
+                    queueOfDigits.Enqueue(input[i]);
+                }
+            }
+
+            Console.Write(string.Join(", " ,queueOfDigits));
+        }
+    }
+}
