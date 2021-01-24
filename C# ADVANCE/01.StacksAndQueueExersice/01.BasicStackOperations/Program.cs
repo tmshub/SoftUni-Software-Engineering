e136c102558e2a0fd8a58893a9a08ae13edf5bb7
tmshub <tmshub@abv.bg>
2021-01-24 18:43:57 +0200

    resolved problem

---
+++ C# ADVANCE/01.StacksAndQueueExersice/01.BasicStackOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackAndQueueExersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if(!stack.Contains(x) && stack.Count>0)
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var item in stack)
+                {
+                    if (minValue > item)
+                    {
+                        minValue = item;
+                    }
+                }
+
+                Console.WriteLine(minValue);
+            }
+            else if(stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+
+        }
+    }
+}
