f04034d3bc9e583e01bd5a9abb8525f079b8cf9f
tmshub <tmshub@abv.bg>
2021-01-24 18:39:12 +0200

    resolved problems

---
+++ C# ADVANCE/01.StacksAndQueueLab/07.HotPatato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPatato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] childrens = Console.ReadLine().Split();
+            int numOfPassed = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(childrens);
+            int count = 0;
+
+            while (queue.Count > 1)
+            {
+                count++;
+
+                if(count == numOfPassed)
+                {
+                    count = 0;
+                    Console.WriteLine("Removed "+queue.Dequeue());
+                }
+                else
+                {
+                    string firstChildren = queue.Dequeue();
+                    queue.Enqueue(firstChildren);
+                }
+            }
+            Console.WriteLine("Last is "+queue.Dequeue());
+        }
+    }
+}
---
+++ C# ADVANCE/01.StacksAndQueueLab/08.TarafficJam/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TarafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queueOfCars = new Queue<string>();
+
+            string command = Console.ReadLine();
+            int countOfPassedCars = 0;
+
+            while (command!="end")
+            {
+                if(command == "green")
+                {
+                    if (queueOfCars.Count >= n)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.WriteLine($"{queueOfCars.Dequeue()} passed!");
+                            countOfPassedCars++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < queueOfCars.Count; i++)
+                        {
+                            Console.WriteLine($"{queueOfCars.Dequeue()} passed!");
+                            countOfPassedCars++;
+                            i = -1;
+                        }
+                    }
+                }
+                else
+                {
+                    queueOfCars.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(countOfPassedCars+" cars passed the crossroads.");
+        }
+    }
+}
