0aa7970161219800c054712b25f1bc0e460d9d22
tmshub <tmshub@abv.bg>
2021-01-30 21:35:27 +0200

    resolved problems

---
+++ C# ADVANCE/03.SetsAndDictionariesAdvancedExersice/02.SetsOfElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+            int firstSetCount = n[0];
+            int secondSetCount = n[1];
+
+            for (int i = 0; i < firstSetCount; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                firstSet.Add(input);
+            }
+            for (int i = 0; i < secondSetCount; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (firstSet.Contains(input))
+                {
+                    secondSet.Add(input);
+                }
+            }
+
+            foreach (var item in firstSet)
+            {
+                if(firstSet.Contains(item) && secondSet.Contains(item))
+                {
+                    Console.Write(item+" ");
+                }
+            }
+        }
+    }
+}
---
+++ C# ADVANCE/03.SetsAndDictionariesAdvancedExersice/03.PeriodicTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> chemicalElements = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    chemicalElements.Add(input[j]);
+                }
+            }
+         
+            foreach (var item in chemicalElements.OrderBy(k => k))
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
