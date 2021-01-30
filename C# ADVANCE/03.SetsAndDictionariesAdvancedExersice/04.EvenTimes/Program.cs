8d4017e466a5459d53feb9167d161f92d8cc7330
tmshub <tmshub@abv.bg>
2021-01-30 21:36:33 +0200

    resolved problems

---
+++ C# ADVANCE/03.SetsAndDictionariesAdvancedExersice/04.EvenTimes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (dictionary.ContainsKey(input))
+                {
+                    dictionary[input]++;
+                }
+                else
+                {
+                    dictionary.Add(input,1);
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
---
+++ C# ADVANCE/03.SetsAndDictionariesAdvancedExersice/05.CountSymbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (dictionary.ContainsKey(text[i]))
+                {
+                    dictionary[text[i]]++;
+                }
+                else
+                {
+                    dictionary.Add(text[i], 1);
+                }
+
+            }
+            foreach (var item in dictionary.OrderBy(k => k.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
