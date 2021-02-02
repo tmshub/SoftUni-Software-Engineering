16830ff6af126a78b62512e9def23fa398ef9858
tmshub <tmshub@abv.bg>
2021-02-02 19:17:17 +0200

    resolved problems

---
+++ C# ADVANCE/04.StreamsFilesAndDirectoriesLab/04.MergeFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstFile = File.ReadAllLines("../../../FileOne.txt");
+            string[] secondFile = File.ReadAllLines("../../../FileTwo.txt");
+
+            using (StreamWriter writer = new StreamWriter("../../../FileThree.txt"))
+            {
+                for (int i = 0; i < firstFile.Length; i++)
+                {
+                    writer.WriteLine(firstFile[i]);
+                    writer.WriteLine(secondFile[i]);
+                }
+            }
+        }
+    }
+}
