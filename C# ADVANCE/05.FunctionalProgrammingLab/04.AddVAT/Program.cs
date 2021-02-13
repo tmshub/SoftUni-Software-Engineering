fae126ca2b4bc7da01e064e876288b457575cffc
tmshub <tmshub@abv.bg>
2021-02-13 18:49:39 +0200

    resolved problems

---
+++ C# ADVANCE/05.FunctionalProgrammingLab/04.AddVAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine().Split(", ")
+                .Select(double.Parse)
+                .Select(n => n*1.20)
+                .ToArray();
+           
+            foreach (var item in prices)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+
+
+        }
+    }
+}
