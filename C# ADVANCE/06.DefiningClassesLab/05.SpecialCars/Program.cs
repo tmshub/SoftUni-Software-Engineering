72d57816ed8e1512d65e7a4187470ac4e6ab4328
tmshub <tmshub@abv.bg>
2021-02-18 18:51:36 +0200

    resolved problem

---
+++ C# ADVANCE/06.DefiningClassesLab/05.SpecialCars/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarManufacturer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] tiresInfo = Console.ReadLine().Split();
+            Dictionary<string, Tire[]> tires = new Dictionary<string, Tire[]>();
+            int count = 1;
+
+            while (tiresInfo[0]!="No more tires")
+            {
+                for (int i = 0; i < tiresInfo.Length; i+=2)
+                {
+                    int year = int.Parse(tiresInfo[i]);
+                    double pressure = double.Parse(tiresInfo[i+1]);
+                    tires.Add("tire" + count.ToString(), new Tire[8]);
+
+                    tires["tire" + count.ToString()][i] = year;
+                }
+                
+
+                
+            }
+        }
+    }
+}
