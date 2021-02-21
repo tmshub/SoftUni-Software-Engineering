1edc812658238e8b85a90d294927389c04b99149
tmshub <tmshub@abv.bg>
2021-02-22 00:30:52 +0200

    resolved problems

---
+++ C# ADVANCE/03.SetsAndDictionariesAdvancedLab/06.ParkingLot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ");
+            HashSet<string> cars = new HashSet<string>();
+
+            while (input[0]!="END")
+            {
+                string command = input[0];
+                string carNumber = input[1];
+
+                if(command == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else
+                {
+                    cars.Remove(carNumber);
+                }
+
+                input = Console.ReadLine().Split(", ");
+            }
+
+            if(cars.Count > 0)
+            {
+                foreach (var item in cars)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ C# ADVANCE/03.SetsAndDictionariesAdvancedLab/07.SoftUniParty/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuest = new HashSet<string>();
+            HashSet<string> normalGuest = new HashSet<string>();
+            HashSet<string> guests = new HashSet<string>();
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                guests.Add(input);
+                input = Console.ReadLine();
+            }
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (guests.Contains(input))
+                {
+                    guests.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Count);
+            foreach (var item in guests)
+            {
+                if (char.IsDigit(item[0]))
+                {
+                    vipGuest.Add(item);
+                }
+                else
+                {
+                    normalGuest.Add(item);
+                }
+            }
+            if (vipGuest.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, vipGuest));
+            }
+            if (normalGuest.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, normalGuest));
+            }
+        }
+    }
+}
