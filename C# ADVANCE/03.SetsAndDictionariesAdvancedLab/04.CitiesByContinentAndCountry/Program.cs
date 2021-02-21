3ca5475976aa15daffab49566763ff55941cf7fd
tmshub <tmshub@abv.bg>
2021-02-21 23:54:48 +0200

    resolved problem

---
+++ C# ADVANCE/03.SetsAndDictionariesAdvancedLab/04.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> dictionary = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!dictionary.ContainsKey(continent))
+                {
+                    dictionary.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!dictionary[continent].ContainsKey(country))
+                {
+                    dictionary[continent].Add(country, new List<string>());
+                }
+                dictionary[continent][country].Add(city);
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key + ":");
+
+                foreach (var country in item.Value)
+                {
+                    Console.Write($" {country.Key} -> ");
+                    
+                    Console.WriteLine(string.Join(", ", country.Value));
+                }
+            }
+        }
+    }
+}
