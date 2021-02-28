d009c6d2ee8ea36904d73da69309c98404f69dd5
tmshub <tmshub@abv.bg>
2021-02-28 19:07:22 +0200

    resolved problems

---
+++ C# OOP/02.EncapsulationLab/04.FirstAndReserveTeam/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary = 460)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { 
+            get => this.firstName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+
+                }
+
+               this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => this.lastName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+
+                }
+
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get => this.age;
+            private set
+            {
+                if (value < 1)
+                {
+
+                }
+
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => this.salary;
+            private set
+            {
+                if (value<460)
+                {
+
+                }
+
+                this.salary= value;
+            }
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage = percentage / 2;
+            }
+           
+          Salary = Salary + Salary * percentage / 100;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ C# OOP/02.EncapsulationLab/04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        public Team(string name)
+        {
+            this.name = name;
+           firstTeam = new List<Person>();
+           reserveTeam = new List<Person>();
+        }
+            private string name;
+            private List<Person> firstTeam;
+            private List<Person> reserveTeam;
+
+        public ReadOnlyCollection<Person> FirstTeam 
+        { 
+            get
+            {
+                return this.firstTeam.AsReadOnly();
+            } 
+        }
+
+        public ReadOnlyCollection<Person> ReserveTeam
+        {
+            get
+            {
+                return this.firstTeam.AsReadOnly();
+            }
+            
+        }
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                firstTeam.Add(player);
+            }
+            else
+            {
+                reserveTeam.Add(player);
+            }
+        }
+    }
+}
