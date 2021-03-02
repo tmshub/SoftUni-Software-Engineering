42a4b8f65215bd0df9a9262aa5e5ac06908c9c69
tmshub <tmshub@abv.bg>
2021-03-02 15:07:54 +0200

    lecture exersice problems

---
+++ C# ADVANCE/08.GenericsLab/01.BoxOfT/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        List<T> Item;
+
+        public Box()
+        {
+            Item = new List<T>();
+        }
+        public int Count { get { return Item.Count; } }
+
+        public void Add(T element)
+        {
+            Item.Insert(0, element);
+        }
+
+        public T Remove()
+        {
+            T element = Item[0];
+            Item.RemoveAt(0);
+            return element;
+        }
+    }
+}
---
+++ C# ADVANCE/08.GenericsLab/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T1, T2> 
+        where T1 : IComparable
+        where T2 : IComparable
+    {
+        public EqualityScale(T1 firstElement, T2 secondElement)
+        {
+            FirstElement = firstElement;
+            SecondElement = secondElement;
+        }
+
+        public T1 FirstElement { get; set; }
+        public T2 SecondElement { get; set; }
+
+        public bool AreEqual()
+        {
+            if(FirstElement.CompareTo(SecondElement) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
