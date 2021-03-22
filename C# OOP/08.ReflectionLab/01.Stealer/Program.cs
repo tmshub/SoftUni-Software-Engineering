4e9e881abddd0425c723b456c90df0ada20921be
tmshub <tmshub@abv.bg>
2021-03-22 21:02:16 +0200

    add new lecture

---
+++ C# OOP/08.ReflectionLab/01.Stealer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Stealer;
+using System;
+using System.Reflection;
+
+namespace Stealer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            var result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C# OOP/08.ReflectionLab/01.Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+   public class Spy
+    {
+        public string StealFieldInfo(string name, params string[] data)
+        {
+            Type type = Type.GetType(name);
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            StringBuilder sb = new StringBuilder();
+            Object classIstance = Activator.CreateInstance(type, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {classIstance}");
+
+            foreach (FieldInfo field in fields.Where(x => data.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classIstance)}");
+            }
+
+            Console.WriteLine(sb);
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
